c8381e98ca6704cd11b42a42d770de874335bf2a
Heo Ji Sung <jsheo@postech.ac.kr>
2018-09-30 19:24:38 +0900

    fundam

--- main.cpp
+++ main.cpp
@@ -2,18 +2,52 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 using namespace std;
-void renderScene(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glFlush();
+extern int map[20][20];
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glLoadIdentity();
+	gluOrtho2D(0,500,0,400);
 }
-void main(int argc, char **argv) {
+
+void drawWall(int i, int j)
+{
+	glColor3f(1,0,0);
+	glRectf(i*50, j*50, (i+1)*50, (j+1)*50);
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	for (int i = 0; i < 20; i++)
+		for (int j = 0; j < 20; j++)
+			if (map[i][j] == 0)
+				drawWall(i, j);
+	glutSwapBuffers();
+}
+
+void special(int key, int x, int y)
+{
+	switch (key) {
+	case GLUT_KEY_UP:
+		break;
+	case GLUT_KEY_DOWN:
+		break;
+	case GLUT_KEY_LEFT:
+		break;
+	case GLUT_KEY_RIGHT:
+		break;
+	}
+}
+void main(int argc, char **argv)
+{
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(1000, 1000);
-	glutCreateWindow("Hello OpenGL");
-	glutDisplayFunc(renderScene);
-	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
-	glewInit();
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutCreateWindow("simple");
+	glutSpecialFunc(special);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+
 	glutMainLoop();
 }
